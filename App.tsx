2f5172fdd98c61d7260b737079c2f33de5c7f2fd
Melissa Gomes <melissa.gomes@greenwave.ind>
2023-04-26 09:05:59 -0300

    atualização

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { Container, Programador, AreaVisor, Visor, Caracteres } from "./styles";
+import { Container, Programador, AreaVisor, Visor, Caracteres, AreaButtons, RowButtons, Button, TitleButton, Titulo } from "./styles";
+import { TouchableOpacity } from "react-native";
 
 export default function App() {
     return (
@@ -8,9 +9,105 @@ export default function App() {
             <Programador>
                 <AreaVisor>
                     <Visor>
-                        <Caracteres>123456</Caracteres>
+                        <Caracteres>123</Caracteres>
                     </Visor>
                 </AreaVisor>
+                <Titulo>PROGRAMADOR</Titulo>
+                <AreaButtons>
+
+                    <RowButtons>
+                        <TouchableOpacity>
+                            <Button color='#fff'>
+                                <TitleButton>7</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#fff'>
+                                <TitleButton>8</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#fff'>
+                                <TitleButton>9</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#646060'>
+                                <TitleButton>MF</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                    </RowButtons>
+
+                    <RowButtons>
+                        <TouchableOpacity>
+                            <Button color='#ec9898f9'>
+                                <TitleButton>4</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#e9eb9f'>
+                                <TitleButton>5</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#a3d898'>
+                                <TitleButton >6</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#646060'>
+                                <TitleButton style={{ color: '#fceb02' }}>X</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                    </RowButtons>
+
+                    <RowButtons>
+                        <TouchableOpacity>
+                            <Button color='#db1414'>
+                                <TitleButton>1</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#e2e61c'>
+                                <TitleButton>2</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#2fb307'>
+                                <TitleButton>3</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#646060'>
+                                <TitleButton>FIM</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                    </RowButtons>
+
+                    <RowButtons>
+                        <TouchableOpacity>
+                            <Button color='#fff'>
+                                <TitleButton>0</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#646060'>
+                                <TitleButton>AL</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#646060'>
+                                <TitleButton>AC</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Button color='#646060'>
+                                <TitleButton style={{ color: '#fceb02' }}>E</TitleButton>
+                            </Button>
+                        </TouchableOpacity>
+                    </RowButtons>
+
+                </AreaButtons>
             </Programador>
         </Container>
     );
--- styles.tsx
+++ styles.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components/native";
 
+type colorProps = {
+    color?: string
+}
+
 export const Container = styled.View`
     background: #0f0f0f;
     flex: 1;
@@ -13,6 +17,7 @@ export const Programador = styled.View`
     height: 90%;
     background: #383737;
     border-radius: 7px;
+    gap: 5px;
 `;
 
 export const AreaVisor = styled.View`
@@ -24,15 +29,57 @@ export const AreaVisor = styled.View`
 `;
 
 export const Visor = styled.View`
-    background: #475742;
+    background: #caceb8;
     width: 85%;
     height: 60px;
-    margin: 20px auto;
+    margin: 30px auto;
     justify-content: center;
-    padding: 0 7px;
+    padding: 0 4px;
 `;
 
 export const Caracteres = styled.Text`
     font-size: 30px;
     letter-spacing: 2px;
+    justify-content: center;
 `;
+
+export const Titulo = styled.Text`
+    font-size: 30px;
+    letter-spacing: 2px;
+    justify-content: center;
+    margin: -20px auto -7px;
+    color: #fff;
+    font-weight: 800;
+`
+
+export const AreaButtons = styled.View`
+    border: 5px solid #888686;
+    border-radius: 12px;
+    width: 90%;
+    height: 43%;
+    margin: 15px auto;
+`;
+
+export const RowButtons = styled.View`
+    padding: 0 15px;
+    margin-top: 15px;
+    flex-direction: row;
+    justify-content: center;
+    gap: 18px;
+`;
+
+export const Button = styled.View<colorProps>`
+    width: 55px;
+    height: 55px;
+    background: ${props => props.color ? props.color : "#d6d2d2"};
+    align-items: center;
+    justify-content: center;
+`;
+
+export const TitleButton = styled.Text`
+    font-size: 30px;
+    font-weight: 600;
+`;
+
+
+
